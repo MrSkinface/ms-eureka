eb970e75636effdb037e96cf2e12984a368a0746
Mike <levitskym@edin.ua>
2022-06-17 17:15:54 +0300

    close #5

---
+++ src/main/java/ua/mike/micro/eureka/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package ua.mike.micro.eureka.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * Created by mike on 17.06.2022 15:57
+ */
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic();
+    }
+}
